b27e919b19e007dda526a6a5df02386bc7059c07
Triobhuwan Pal <tribhuwanpal1729@gmail.com>
2024-03-27 14:45:13 +0530

    vgtde

--- src/main/java/JenkinsTest/jenkinstest/controller/UserController.java
+++ src/main/java/JenkinsTest/jenkinstest/controller/UserController.java
@@ -8,6 +8,9 @@ public class UserController {
     @GetMapping("/getUser")
     public String getUser()
     {
+        System.out.println("Hi I am New +++++++++++++++++++++++++++++++++++++++++++++++++++++++");
         return "This is main page";
     }
 }
+//
+//C:\Users\tribhuvan pal\Desktop\jenkins-test\target\jenkins-test-0.0.1-SNAPSHOT.war
